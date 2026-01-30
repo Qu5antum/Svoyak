da7b211b3f2adf6d3809f4341b5da6ca482f2580
Qu5antum <erae88959@gmail.com>
2026-01-31 00:04:20 +0300

    index.js

---
+++ public/backend/index.js
@@ -0,0 +1,34 @@
+import { auth } from "./firebase.js";
+import { signInAnonymously } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const input = document.getElementById("nameInput");
+const btn = document.getElementById("startBtn");
+
+// если имя уже есть — сразу в лобби
+const savedName = localStorage.getItem("playerName");
+if (savedName) {
+  window.location.href = "lobby.html";
+}
+
+btn.addEventListener("click", async () => {
+  const name = input.value.trim();
+
+  if (name.length < 2) {
+    alert("Имя должно быть минимум 2 символа");
+    return;
+  }
+
+  try {
+    
+    await signInAnonymously(auth);
+
+    localStorage.setItem("playerName", name);
+
+    window.location.href = "lobby.html";
+
+  } catch (err) {
+    alert("Ошибка входа");
+    console.error(err);
+  }
+});
